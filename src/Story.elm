cbf8c1529ce2ed3941107526c0f66fc9c4ceb3ed
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-10-04 23:14:17 +0200

    WithElements using records

--- src/Story.elm
+++ src/Story.elm
@@ -7,7 +7,7 @@ import Set exposing (Set)
 type Story
     = EmptyStory
     | WithFlaw { flaw : String }
-    | WithElements String String String String
+    | WithElements { flaw : String, elements : List String }
     | CompleteStory
         { flaw : String
         , elements : Set String
@@ -41,10 +41,11 @@ getFlaw story =
         EmptyStory ->
             Nothing
 
-        WithFlaw a -> Just a.flaw
+        WithFlaw a ->
+            Just a.flaw
 
-        WithElements flaw _ _ _ ->
-            Just flaw
+        WithElements a ->
+            Just a.flaw
 
         CompleteStory s ->
             Just s.flaw
@@ -53,8 +54,8 @@ getFlaw story =
 setElements : String -> String -> String -> Story -> Maybe Story
 setElements el1 el2 el3 story =
     case story of
-        WithFlaw {flaw} ->
-            Just (WithElements flaw (String.toLower el1) (String.toLower el2) (String.toLower el3))
+        WithFlaw a ->
+            Just (WithElements { flaw = a.flaw, elements = List.map String.toLower [ el1, el2, el3 ] })
 
         _ ->
             Nothing
@@ -63,6 +64,8 @@ setElements el1 el2 el3 story =
 getElements : Story -> Maybe (List String)
 getElements story =
     case story of
-        WithElements _ el1 el2 el3 -> Just [el1, el2, el3]
-        _ -> Nothing
-            
+        WithElements a ->
+            Just a.elements
+
+        _ ->
+            Nothing
